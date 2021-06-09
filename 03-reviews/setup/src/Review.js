9841c967eefef7d29f2e9abcdba9667748497371
tahsinAmin <muhammadtahsinamin@gmail.com>
2021-06-09 23:31:12 +0600

    3-Reviews Setup

--- 03-reviews/setup/src/Review.js
+++ 03-reviews/setup/src/Review.js
@@ -3,7 +3,32 @@ import people from './data';
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
 const Review = () => {
-  return <h2>review component</h2>;
+  const [index, setIndex] = useState(0);
+  const {name, job, image, text} = people[index];
+  console.log(people.length);
+  return <article className="review">
+    <div className="img-container">
+      <img src={image} alt={name} className='person-img' onClick={() => setIndex((index+1)%people.length)}/>
+      <span className="quote-icon">
+        <FaQuoteRight />
+      </span> 
+    </div>
+    <h4 className="author">{name}</h4>
+    <p className="job">{job}</p>
+    <p className="info">{text}</p>
+    <div className="button-container">
+      <button className='prev-btn'>
+        <FaChevronLeft />
+      </button>
+          <button className='next-btn'>
+        <FaChevronRight />
+      </button>
+    </div>
+    <button className='random-btn'>
+        surprise me
+      </button>
+  </article>;
 };
 
 export default Review;
+{/* <button className="btn" onClick={() => setIndex(Math.floor(Math.random()*people.length))}>Surprise Me</button> */}
